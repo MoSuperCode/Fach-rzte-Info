e76ee8301855dbc5f7b7aeb8259708c69aacbfd2
mosupercode <mo.supercode@gmail.com>
2024-04-17 22:41:20 +0200

    Initial Commit: Programm sends data to backend -> server receives data & writes in a File

---
+++ Backend/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer();
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/submit-form", upload.none(), (req, res) => {
+  let count = 1;
+  let dataToSave = "";
+
+  const name = req.body.name;
+  const email = req.body.select;
+  const protokollNummer = req.body["protokoll-nummer"];
+
+  for (
+    let count = 1;
+    req.body["untersuchung" + count] && req.body["wert" + count];
+    count++
+  ) {
+    const untersuchung = req.body["untersuchung" + count];
+    const wert = req.body["wert" + count];
+    dataToSave += `Untersuchung ${count}: ${untersuchung}, Wert ${count}: ${wert}\n`;
+  }
+
+  dataToSave = `Namenskürzel: ${name},\nEmpfänger E-Mail: ${email},\nProtokollnummer: ${protokollNummer},\nUntersuchung ${count}: ${untersuchung}, Wert ${count}: ${wert}\n`;
+  // Hier erstelle ich ein Zeitstempel und füge ihn in den File - Namen ein
+  const timestamp = new Date().toISOString().replace(/:/g, "-");
+  const filePath = `./data/facharzt-info-${timestamp}.txt`;
+
+  fs.writeFile(filePath, dataToSave, (err) => {
+    if (err) {
+      console.error("Fehler beim Speichern der Daten");
+      res.status(500).send("Fehler beim Speichern der Daten.");
+    } else {
+      console.log("SUCCESS");
+      res.send("Daten wurden erfolgreich als Datei gespeichert");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
---
+++ Frontend/js/main.js
@@ -0,0 +1,113 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  const responseMessage = document.getElementById("responseMessage");
+  const addButton = document.getElementById("addMore");
+  let idCounter = 1;
+
+  addButton.addEventListener("click", function () {
+    // idCounter zum aufzählen der ids
+    idCounter++;
+
+    // Div wird erstellt
+    const neuerDiv = document.createElement("div");
+    neuerDiv.className = "mb-3";
+
+    const flexDiv = document.createElement("div");
+    flexDiv.className = "flex";
+
+    // Erstelle das Label für das neue Untersuchungsfeld
+    const neuesLabelUnt = document.createElement("label");
+    neuesLabelUnt.htmlFor = "untersuchung" + idCounter;
+    neuesLabelUnt.className = "form-label";
+    neuesLabelUnt.textContent = "Untersuchung: ";
+
+    // Input für neue Untersuchung wird erstellt, Attribute werden hinzugefügt
+    const neueUntersuchung = document.createElement("input");
+    neueUntersuchung.type = "text";
+    neueUntersuchung.className = "form-control";
+    neueUntersuchung.id = "untersuchung" + idCounter;
+    neueUntersuchung.name = "untersuchung" + idCounter;
+    neueUntersuchung.required = true;
+
+    // Erstelle das Label für das neue Wertfeld
+    const neuesLabelVal = document.createElement("label");
+    neuesLabelVal.htmlFor = "wert" + idCounter;
+    neuesLabelVal.className = "form-label";
+    neuesLabelVal.textContent = "Wert: ";
+
+    // Input für neuen Wert wird erstellt, attribute werden hinzugefügt
+    const neuerWert = document.createElement("input");
+    neuerWert.type = "text";
+    neuerWert.className = "form-control";
+    neuerWert.id = "value" + idCounter;
+    neuerWert.name = "value" + idCounter;
+    neuerWert.required = true;
+
+    // Lösch-Button erstellen
+    const neuerLöschButton = document.createElement("button");
+    neuerLöschButton.type = "button";
+    neuerLöschButton.className = "btn btn-danger btn-sm mb-2 lösch"; // Rote Farbe und kleine Größe
+    neuerLöschButton.textContent = "X";
+
+    // EventListener für den Lösch-Button hinzufügen
+    neuerLöschButton.addEventListener("click", function () {
+      form.removeChild(neuerDiv); // Entfernt das gesamte Div
+    });
+
+    // Elemente werden ins Div eingefügt
+    flexDiv.appendChild(neuesLabelUnt);
+    flexDiv.appendChild(neuerLöschButton);
+    neuerDiv.appendChild(flexDiv);
+    neuerDiv.appendChild(neueUntersuchung);
+    neuerDiv.appendChild(neuesLabelVal);
+    neuerDiv.appendChild(neuerWert);
+
+    // neuerDiv wird vor dem addMore Butten eingesetzt
+    form.insertBefore(neuerDiv, addButton);
+  });
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const protokollInput = document.getElementById("protokoll-nummer").value;
+    const select = document.getElementById("select").value;
+    const errorText = document.getElementById("protokollError");
+    const responseMessage = document.getElementById("responseMessage");
+
+    // Protokollnummer-Validierung (muss genau 7 Ziffern lang sein)
+    if (protokollInput.length !== 7) {
+      errorText.textContent =
+        "Die Protokollnummer muss genau sieben Ziffern lang sein.";
+      return; // Stoppt das Absenden des Formulars
+    } else {
+      errorText.textContent = ""; // reseted fehlermessage, falls die Eingabe korrekt ist
+    }
+
+    if (select === "") {
+      alert("Bitte wählen Sie einen gültigen Absender aus.");
+      return;
+    }
+
+    const formData = new FormData(form);
+
+    // Sendet die Daten asynchron zum Server, d.h. muss nicht auf ein response warten
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((text) => {
+        responseMessage.textContent = text; // Zeigt die Antwort vom Server an
+        responseMessage.className = "response-message success"; // Setzt die Klasse für Erfolg
+        setTimeout(form.reset(), 1500); // Setzt das Formular nach dem Absenden zurück, damit man direkt weiterschreiben kann.
+      })
+      .catch((error) => {
+        console.error("Fehler beim Senden der Daten: ", error);
+        responseMessage.textContent = "Fehler beim Senden der Daten.";
+        responseMessage.className = "response-message error"; // -" "- Fehler
+      });
+  });
+});
+
+// Danke fürs durchlesen meines Codes :*
